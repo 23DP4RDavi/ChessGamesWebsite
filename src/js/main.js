3d38abf2062ac1445ee41c53002e94bdb9c17829
Roberts <A230657rd@rvt.lv>
2025-03-31 09:12:37 +0300

    small changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const heroSection = document.getElementById("hero");
-    const chessPieces = ["pawn.png", "knight.png", "bishop.png", "rook.png", "queen.png", "king.png","bpawn.png", "bknight.png", "bbishop.png", "brook.png", "bqueen.png", "bking.png"];
+    const chessPieces = ["pawn.png", "knight.png", "bishop.png", "rook.png", "queen.png", "king.png", "bpawn.png", "bknight.png", "bbishop.png", "brook.png", "bqueen.png", "bking.png"];
 
     function createFallingPiece() {
         const piece = document.createElement("img");
@@ -8,7 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
         piece.classList.add("falling-piece");
 
         piece.style.left = Math.random() * 100 + "vw";
+
         piece.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+        const horizontalMovement = Math.random() * 200 - 100;
+        piece.style.setProperty("--horizontal-movement", `${horizontalMovement}px`);
+
         piece.style.transform = `rotate(${Math.random() * 360}deg)`;
 
         heroSection.appendChild(piece);
